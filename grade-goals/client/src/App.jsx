709c52fee5a97199a48a87ad8a5cf75e40913e09
Abraham Han <seunghan77@gmail.com>
2023-02-18 16:34:05 -0600

    added NavBar

--- grade-goals/client/src/App.jsx
+++ grade-goals/client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ResponsiveAppBar from './ResponsiveNavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+        <ResponsiveAppBar />
       <p>Hello World!</p>     
     </div>
   )
